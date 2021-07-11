14e352d5f379f97d7434b2efee6f3ae0043c920a
Elise Beall <elisebeall@gmail.com>
2021-07-11 17:31:29 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,12 +1,63 @@
-function createTitle() {
-  
-}
-
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
+}
+
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name : name,
+    age : age,
+    pronouns : pronouns
+  }
+
+  return character;
+}
+
+function saveReview(newReview, reviews) {
+  if (reviews.length === 0) {
+    reviews[reviews.length] = newReview;
+    return reviews;
+  } else {
+      for (var i=0; i<reviews.length; i++) {
+        if (reviews[i] === newReview) {
+          return reviews;
+        } else {
+          reviews[reviews.length] = newReview;
+          return reviews;
+        }
+      }
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length*20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+
+  var book = {
+    title : bookTitle,
+    mainCharacter : bookCharacter,
+    pageCount : calculatePageCount(bookTitle),
+    genre: genre
+  }
+
+  return book;
+}
+
+function editBook(book) {
+  var quarterPages = book.pageCount/4;
+  var pages = book.pageCount-quarterPages;
+
+  book.pageCount = pages;
+
+  return book;
 }
