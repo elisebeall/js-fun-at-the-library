3f4acc0de27b143aabfa49d0844996a79f0fb3ae
Elise Beall <elisebeall@gmail.com>
2021-07-11 21:17:34 -0600

    Refactor

--- src/Library.js
+++ src/Library.js
@@ -19,49 +19,18 @@ function createLibrary(name) {
 }
 
 function addBook(library, book) {
-  if (book.genre === "fantasy") {
-    library.shelves.fantasy.push(book);
-  } else if (book.genre === "nonFiction") {
-    library.shelves.nonFiction.push(book);
-  } else if (book.genre === "fiction") {
-    library.shelves.fiction.push(book);
-  }
+  library.shelves[book.genre].push(book);
 
   return library;
 }
 
 function checkoutBook(library, book, shelf) {
-  if (shelf === "fantasy") {
-    if (library.shelves.fantasy.includes(book)) {
-      console.log(`You have now checked out ${book} from the ${library.name}`);
+  for (var i=0; i < library.shelves[shelf].length; i++) {
+    if (library.shelves[shelf][i].title === book) {
+      library.shelves[shelf].splice(i, 1);
 
-      var removeBookIndex = library.shelves.fantasy.indexOf(book);
-
-      return library.shelves.fantasy.splice(removeBookIndex, 1);
-    } else {
-      return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
+      return `You have now checked out ${book} from the ${library.name}`;
     }
-  }
-  if (shelf === "nonFiction") {
-    if (library.shelves.nonFiction.includes(book)) {
-      console.log(`You have now checked out ${book} from the ${library.name}`);
-
-      var removeBookIndex = library.shelves.nonFiction.indexOf(book);
-
-      return library.shelves.nonFiction.splice(removeBookIndex, 1);;
-    } else {
-      return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
-    }
-  }
-  if (shelf === "fiction") {
-    if (library.shelves.fiction.includes(book)) {
-      console.log(`You have now checked out ${book} from the ${library.name}`);
-
-      var removeBookIndex = library.shelves.fiction.indexOf(book);
-
-      return library.shelves.fiction.splice(removeBookIndex, 1);
-    } else {
-      return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
-    }
-  }
-}
+   }
+   return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
+ }
